1b20f1a024a051efad7edde82e4684bdca0eb0a2
iqbalfaisal <7faisaliqbal@gmail.com>
2021-01-28 14:38:12 +0500

    final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
+
+  skip_before_action :authorized, only: [:new, :create]
+
   def new
   end
 
   def create
+
+    @user= User.create(params.require(:user).permit(:username, :password))
+
+    session[:user_id]=@user.id
+
+    redirect_to "/welcome"
+
   end
 end
